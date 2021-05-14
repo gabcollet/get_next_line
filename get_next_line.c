c0d79489d7a76bfbcc79d1f68c32cf283b4d4d5a
Gabriel Collet <gcollet@c2r4p5.42quebec.com>
2021-05-14 15:42:36 -0400

    Premier commit

---
+++ get_next_line.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcollet <gcollet@student.42quebec.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/14 15:27:41 by gcollet           #+#    #+#             */
+/*   Updated: 2021/05/14 15:29:49 by gcollet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* Retourne une ligne lue depuis un fd, sans le retour à la ligne */
+/* Valeur de retour : 
+1 : Une ligne a été lue
+0 : La fin de fichier a été atteinte
+-1 : Une erreur est survenue */
+
+int	get_next_line(int fd, char **line)
+{
+	
+}
